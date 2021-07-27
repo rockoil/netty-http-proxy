976b0be13b933e0ede7f4409295ad596599d05ab
rockoil <rockoil@nate.com>
2021-07-28 00:26:13 +0900

    기본 소스 작업중

--- src/main/java/com/hakine/apps/netty/handler/HttpFrontendHandler.java
+++ src/main/java/com/hakine/apps/netty/handler/HttpFrontendHandler.java
@@ -2,10 +2,8 @@ package com.hakine.apps.netty.handler;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.buffer.Unpooled;
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelFutureListener;
-import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.*;
+import io.netty.handler.codec.http.FullHttpRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,12 +12,84 @@ public class HttpFrontendHandler extends ChannelInboundHandlerAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(HttpFrontendHandler.class);
 
+    private final String remoteHost;
+    private final int remotePort;
+
+    // As we use inboundChannel.eventLoop() when building the Bootstrap this does not need to be volatile as
+    // the outboundChannel will use the same EventLoop (and therefore Thread) as the inboundChannel.
+    private Channel outboundChannel;
+
+    private boolean isSsl;
+
+    public HttpFrontendHandler(String remoteHost, int remotePort) {
+        this.remoteHost = remoteHost;
+        this.remotePort = remotePort;
+        this.isSsl = remoteHost.contains("https");
+    }
+
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        final Channel inboundChannel = ctx.channel();
 
+        // Start the connection attempt.
         Bootstrap b = new Bootstrap();
+        b.group(inboundChannel.eventLoop())
+                .channel(ctx.channel().getClass())
+                .handler(new HttpBackendHandler(inboundChannel))
+                .option(ChannelOption.AUTO_READ, false);
+
+        ChannelFuture f = b.connect(remoteHost, remotePort);
+        outboundChannel = f.channel();
+
+        f.addListener(new ChannelFutureListener() {
+            @Override
+            public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                if(channelFuture.isSuccess()) {
+                    inboundChannel.read();
+                } else {
+                    inboundChannel.close();
+                }
+            }
+        });
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        if(outboundChannel.isActive()) {
+
+            if(msg instanceof FullHttpRequest) {
+                FullHttpRequest request = (FullHttpRequest) msg;
 
 
+
+            }
+
+
+            outboundChannel.writeAndFlush(msg).addListener(new ChannelFutureListener() {
+                @Override
+                public void operationComplete(ChannelFuture channelFuture) throws Exception {
+                    if(channelFuture.isSuccess()) {
+                        // was able to flush out data, start to read the next chunk
+                        ctx.channel().read();
+                    } else {
+                        channelFuture.channel().close();
+                    }
+                }
+            });
+        }
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        if(outboundChannel != null) {
+            closeOnFlush(outboundChannel);
+        }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        cause.printStackTrace();
+        closeOnFlush(ctx.channel());
     }
 
     static void closeOnFlush(Channel ch) {
